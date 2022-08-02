d16cec1b1de2ae2462a69547f05c765afb76ea07
cyblogerz <pranavajay2002@gmail.com>
2022-08-03 00:23:28 +0530

    Created book class

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,1 +1,12 @@
+class Book {
+  final String title;
+  final String author;
+  final int rating;
+  final String url;
 
+  Book(
+      {required this.author,
+      required this.rating,
+      required this.url,
+      required this.title});
+}
