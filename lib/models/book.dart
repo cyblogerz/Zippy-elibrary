d076d365693c5b4c6cec497491b381237427718c
cyblogerz <pranavajay2002@gmail.com>
2022-08-03 12:59:33 +0530

    Updated book model

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -2,11 +2,13 @@ class Book {
   final String title;
   final String author;
   final int rating;
+  final String descr;
   final String url;
 
   Book(
       {required this.author,
       required this.rating,
+      required this.descr,
       required this.url,
       required this.title});
 }
